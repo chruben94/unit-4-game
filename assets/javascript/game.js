3f82f76fa4fa4e4586c75431467aeb3b60706410
Ruben Chavez <chruben94@gmail.com>
2019-10-13 05:31:48 -0700

    Need to make it so that when user clicks on crystal, those numbers are added to the total score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,6 +11,17 @@ var crystal1 = $(".firstCrystal");
 var crystal1Number = Math.floor(Math.random() * 12) + 1;
 console.log(crystal1Number);
 
+var crystal2 = $(".secondCrystal");
+var crystal2Number = Math.floor(Math.random() * 12) + 1;
+console.log(crystal2Number);
+
+var crystal3 = $(".thirdCrystal");
+var crystal3Number = Math.floor(Math.random() * 12) + 1;
+console.log(crystal3Number);
+
+var crystal4 = $(".fourthCrystal");
+var crystal4Number = Math.floor(Math.random() * 12) + 1;
+console.log(crystal4Number);
 });
  
 

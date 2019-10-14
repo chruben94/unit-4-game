a8fbd7349c9d32dd1105d204cbc4d7568a14adaf
Ruben Chavez <chruben94@gmail.com>
2019-10-13 19:15:17 -0700

    making sure that every ime the first crystal is clicked the random number is added to total score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,10 +6,26 @@ $(document).ready(function () {
     $("#randomNumber").append(randomNumber);
 
 //give the crystals random number values between 1 - 12
+// create "score" variable
+var score = 0
 
 var crystal1 = $(".firstCrystal");
 var crystal1Number = Math.floor(Math.random() * 12) + 1;
 console.log(crystal1Number);
+//create onclick function
+$(crystal1).on("click", function () {
+crystal1Number = parseInt(crystal1Number);
+score += crystal1Number; 
+$("#currentScore").html(score);
+if (score === randomNumber) {
+    winner()
+    restart()
+}
+else if (score >= randomNumber) {
+    loser()
+    restart()
+}
+})
 
 var crystal2 = $(".secondCrystal");
 var crystal2Number = Math.floor(Math.random() * 12) + 1;
